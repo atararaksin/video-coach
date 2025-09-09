40d88d71ca590a2cd4f81091904cfcc570a9d739
Andrey Tararaksin <attarara@amazon.com>
2025-09-08 19:32:35 -0700

    WIP

--- src/lapUtils.ts
+++ src/lapUtils.ts
@@ -43,126 +43,34 @@ export function reindexLap(lap: LapData, referenceLap: LapData) {
     }
 }
 
-/*
-function rebuildDatapointsToMatchReferenceLapDatapointsOnDistance(datapoints: Datapoint[], refDatapoints: Datapoint[]): Datapoint[] {
-    const timeStep = 0.005;
-    const interpolatedDatapoints: Datapoint[] = [];
-    const channels = datapoints[0].data.keys();
-    for (let i = 0; i < datapoints.length - 1; i++) {
-        const dp1 = datapoints[i];
-        const dp2 = datapoints[i + 1];
-
-        interpolatedDatapoints.push(dp1);
-
-        for (let time = dp1.time + timeStep; time < dp2.time; time += timeStep) {
-            const intermediateDp = Object.assign({}, dp1);
-            
-            intermediateDp.lat = dp1.lat + (dp2.lat - dp1.lat) * (time - dp1.time) / (dp2.time - dp1.time);
-            intermediateDp.lon = dp1.lon + (dp2.lon - dp1.lon) * (time - dp1.time) / (dp2.time - dp1.time);
-            intermediateDp.speed = dp1.speed + (dp2.speed - dp1.speed) * (time - dp1.time) / (dp2.time - dp1.time);
-
-            for (let channel of channels) {
-                const dp1Val = dp1.data.get(channel);
-                const dp2Val = dp2.data.get(channel);
-                intermediateDp.data.set(channel, dp1Val + (dp2Val - dp1Val) * (time - dp1.time) / (dp2.time - dp1.time));
-            }
-
-            interpolatedDatapoints.push(intermediateDp);
-        }
-    }
-
-    interpolatedDatapoints.push(datapoints[datapoints.length - 1]);
+export function getDatapointIndexForLap(lap: LapData, time: number): number {
+    const timeToDistanceIndexLength = lap.timeToDistanceIndex.length;
+    const timeToDistanceIndexStartTime = lap.timeToDistanceIndex[0].time;
+    const timeToDistanceIndexEndTime = lap.timeToDistanceIndex[timeToDistanceIndexLength - 1].time;
     
-    return refDatapoints.map(refDp => findClosestDatapoint(interpolatedDatapoints, refDp));
+    const timeToDistanceIndexResolution = (timeToDistanceIndexEndTime - timeToDistanceIndexStartTime) / (timeToDistanceIndexLength - 1);
+    
+    let index = Math.round((time - timeToDistanceIndexStartTime) / timeToDistanceIndexResolution);
+    if (index < 0) index = 0;
+    else if (index >=  timeToDistanceIndexLength) index = timeToDistanceIndexLength - 1;
+
+    // let index = lap.timeToDistanceIndex.find(dp => dp.time >= time).distanceBasedIndex;
+    // if (index == null) index = lap.datapoints[lap.timeToDistanceIndex.length - 1].tine;
+
+    return lap.timeToDistanceIndex[index].distanceBasedIndex;
 }
 
-function rebuildDatapointsToMatchReferenceLapDatapointsOnDistance2(datapoints: Datapoint[], refDatapoints: Datapoint[]): Datapoint[] {
-    const interpolatedDatapoints: Datapoint[] = [];
-
-    for (let refDp of refDatapoints) {
-        interpolatedDatapoints.push(findDatapointInLapWithInterpolation(refDatapoints, refDp, datapoints));
-    }
-    return interpolatedDatapoints;
-}*/
-
 export function getDatapointForLap(lap: LapData, time: number): Datapoint {
-    const timeBetweenDatapoints = lap.lapTime / lap.timeToDistanceIndex.length;
-    const index = Math.min(lap.timeToDistanceIndex.length - 1, Math.round((time - lap.lapStartTime) / timeBetweenDatapoints));
+    const index = getDatapointIndexForLap(lap, time);
     return lap.datapoints[lap.timeToDistanceIndex[index].distanceBasedIndex];
 }
 
 // For a given base lap at a given point in time, gives a datapoint from the reference lap
 // that is distance-matched to the base lap's datapoint
 export function getReferenceDatapointForLap(lap: LapData, time: number, referenceLap: LapData): Datapoint {
-    const timeBetweenDatapoints = lap.lapTime / lap.timeToDistanceIndex.length;
-    const index = Math.min(lap.timeToDistanceIndex.length - 1, Math.round((time - lap.lapStartTime) / timeBetweenDatapoints));
+    const index = getDatapointIndexForLap(lap, time);
     return referenceLap.datapoints[lap.timeToDistanceIndex[index].distanceBasedIndex];
 }
-
-/*// For a given base lap at a given point in time, gives a datapoint from the reference lap
-// that is distance-matched to the base lap's datapoint
-export function getReferenceDatapointForLapWithInterpolation(lap: LapData, time: number, referenceLap: LapData): Datapoint {
-    const timeBetweenDatapoints = lap.lapTime / lap.timeToDistanceIndex.length;
-    const index = (time - lap.lapStartTime) / timeBetweenDatapoints;
-    const indexFloor = Math.floor(index);
-    if (indexFloor == lap.timeToDistanceIndex.length - 1) {
-        // No next datapoint to interpolate with
-        return referenceLap.datapoints[lap.timeToDistanceIndex[indexFloor].distanceBasedIndex];
-    } else if (indexFloor == index) {
-        return referenceLap.datapoints[lap.timeToDistanceIndex[indexFloor].distanceBasedIndex];
-    } else {
-        const ratio = (indexFloor + 1 - index) / (index - indexFloor); // close to 0 when close to dp1
-        const dp1 = referenceLap.datapoints[lap.timeToDistanceIndex[indexFloor].distanceBasedIndex];
-        const dp2 = referenceLap.datapoints[lap.timeToDistanceIndex[indexFloor + 1].distanceBasedIndex];
-
-        const interpolatedDp = Object.assign({}, dp1);
- 
-        interpolatedDp.time = (dp1.time + ratio * dp2.time) / (1 + ratio);
-        interpolatedDp.speed = (dp1.speed + ratio * dp2.speed) / (1 + ratio);
-        interpolatedDp.lat = (dp1.lat + ratio * dp2.lat) / (1 + ratio);
-        interpolatedDp.lon = (dp1.lon + ratio * dp2.lon) / (1 + ratio);
-        interpolatedDp.data = new Map();
-        for (let channel of dp1.data.keys()) {
-            interpolatedDp.data.set(channel, (dp1.data.get(channel) + ratio * dp2.data.get(channel)) / (1 + ratio));
-        }
-        console.log("Interpolated", dp1, dp2, interpolatedDp);
-        return interpolatedDp; 
-    }
-}
-
-// For a given base lap at a given point in time, gives a datapoint from the reference lap
-// that is distance-matched to the base lap's datapoint, with interpolation
-export function getInterpolatedReferenceDatapointForLap(lap: LapData, datapoint: Datapoint, referenceLap: LapData): Datapoint {
-    const timeBetweenDatapoints = lap.lapTime / lap.timeToDistanceIndex.length;
-    const index = Math.min(lap.timeToDistanceIndex.length - 1, Math.round((datapoint.time - lap.lapStartTime) / timeBetweenDatapoints));
-    const referenceLapIndex = lap.timeToDistanceIndex[index].distanceBasedIndex;
-
-    if (referenceLapIndex == 0 || referenceLapIndex == referenceLap.datapoints.length - 1) {   
-        return referenceLap.datapoints[referenceLapIndex];
-    }
-
-    // Interpolation
-    const candidateDatapoints: Datapoint[] = [];
-    for (let i = referenceLapIndex - 1; i < referenceLapIndex + 1; i++) {
-        candidateDatapoints.push(referenceLap.datapoints[i]);
-    }
-    candidateDatapoints.sort((a, b) => Math.abs(calculateDistance(a.lat, a.lon, datapoint.lat, datapoint.lon)) -  Math.abs(calculateDistance(b.lat, b.lon, datapoint.lat, datapoint.lon)));
-    const dp1 = candidateDatapoints[0];
-    const dp2 = candidateDatapoints[1];
-
-    const interpolatedDp = Object.assign({}, dp1);
-    const distanceRatio = Math.abs(calculateDistance(dp2.lat, dp2.lon, datapoint.lat, datapoint.lon)) / Math.abs(calculateDistance(dp1.lat, dp1.lon, datapoint.lat, datapoint.lon));
-    
-    interpolatedDp.time = (dp2.time + distanceRatio * dp1.time) / (1 + distanceRatio);
-    interpolatedDp.speed = (dp2.speed + distanceRatio * dp1.speed) / (1 + distanceRatio);
-    interpolatedDp.lat = (dp2.lat + distanceRatio * dp1.lat) / (1 + distanceRatio);
-    interpolatedDp.lon = (dp2.lon + distanceRatio * dp1.lon) / (1 + distanceRatio);
-    interpolatedDp.data = new Map();
-    for (let channel of dp1.data.keys()) {
-        interpolatedDp.data.set(channel, (dp2.data.get(channel) + distanceRatio * dp1.data.get(channel)) / (1 + distanceRatio));
-    } 
-    return interpolatedDp;  
-}*/
 
 export function calculateBestTheoreticalLap(laps: LapData[], referenceLap: LapData): LapData {
     laps = laps.filter(l => l.isComplete);
